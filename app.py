21aba98dbe89a3c8dba2a689020e7ffa7f9ab096
Adrien Leib <adrienleib@MacBook-Pro-de-Adrien.local>
2022-03-25 15:42:40 +0100

    first swagger version

--- app.py
+++ app.py
@@ -2,12 +2,16 @@ from flask import Flask
 from flask_mongoengine import MongoEngine
 from routes.serieRoute import series_route
 from routes.filmRoute import films_route
+from routes.episodeRoute import episodes_route
+from  routes.swaggerRoute import swagger_route
 
 
 app = Flask(__name__)
 app.config.from_object('config')
+app.register_blueprint(episodes_route, url_prefix='/episodes')
 app.register_blueprint(series_route, url_prefix='/series')
 app.register_blueprint(films_route, url_prefix='/films')
+app.register_blueprint(swagger_route)
 db = MongoEngine()
 db.init_app(app)
 
--- routes/filmRoute.py
+++ routes/filmRoute.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 from controllers.filmController import get_films,get_film, create_film, update_film, delete_film
-
+#from flask_restplus import Api, Resource
 
 
 films_route = Blueprint('films_route', __name__)
@@ -9,3 +9,4 @@ films_route.route('/', methods=['POST'])(create_film)
 films_route.route('/<id>', methods=['GET'])(get_film)
 films_route.route('/<id>', methods=['PUT'])(update_film)
 films_route.route('/<id>', methods=['DELETE'])(delete_film)
+#api = Api(films_route)
